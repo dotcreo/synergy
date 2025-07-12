901ae5ffbc2f2d2f9312d6da48c06a59b865f534
dotcreo <dotcreo@yandex.ru>
2025-07-12 17:47:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+lst = [1, 2, 3]
 @app.route("/")
 def index():
 	return render_template('index.html')
 
+my_dict = {}
+@app.route("/note", methods=["GET", "POST"])
+def note():
+	if request.method == 'POST':
+		title = request.form['title']
+		string = request.form['string']
+		my_dict[title] = string
+	return render_template('note.html', data=my_dict)
+
 @app.errorhandler(404)
 def not_found(e):
-	return render_template('404.html', 404)
+	return render_template('/templates/404.html', 404)
 
 if __name__ == "__main__":
 	app.run(debug=True)
